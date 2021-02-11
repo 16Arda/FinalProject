9b02f1e8a4d30dd67f6d6a6be7f1934a205487e0
Arda <arda.acar16@hotmail.com>
2021-02-11 13:31:24 +0300

    10 Şubat Ders Sonu

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Entities.Concrete;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "Ürün Eklendi.";
+        public static string ProductNameInvalid = "Ürün ismi geçersiz!";
+        public static string MaintenanceTime = "Bakım Zamanı!";
+        public static string ProductsListed = "Ürünler listelendi.";
+    }
+}

e94682e7fd96255746d75cfe750cc6cc86e78c43
Arda <arda.acar16@hotmail.com>
2021-02-18 18:52:37 +0300

    17 Şubat Ders Sonu

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Text;
 using Business.Abstract;
 using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities.Results;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
 using Entities.Concrete;
 using Entities.DTOs;
+using FluentValidation;
 
 namespace Business.Concrete
 {
@@ -20,14 +24,11 @@ namespace Business.Concrete
             _productDal = productDal;
         }
 
+        [ValidationAspect(typeof(ProductValidator))]
         public IResult Add(Product product)
         {
             // Business Codes
-            if (product.ProductName.Length < 2)
-            {
-                // Magic Strings
-                return new ErrorResult(Messages.ProductNameInvalid);
-            }
+
             _productDal.Add(product);
             return new SuccessResult(Messages.ProductAdded);
         }
--- Entities/Concrete/Product.cs
+++ Entities/Concrete/Product.cs
@@ -12,5 +12,6 @@ namespace Entities.Concrete
         public string ProductName { get; set; }
         public short UnitsInStock { get; set; }
         public decimal UnitPrice { get; set; }
+        public string NationalIdentity { get; set; }
     }
 }
